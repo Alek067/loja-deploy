f135f476d52f1c04c5d2bc037d9ef82ce104070e
Laoma Nogueira <laomanogueira@gmail.com>
2021-11-27 02:21:08 -0300

    Implementadas as models e as controllers dos costumers

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routers = require('./routes')
+const { sequelize } = require('./models');
+const app = express();
+const port = 3001;
+
+app.use(express.json());
+app.use('/', routers)
+
+sequelize.sync().then(() => {
+    console.log('Successfully connected to the database')
+})
+
+app.listen(port, function () {
+    console.log(`Connected server on port: ${port}`);
+});
